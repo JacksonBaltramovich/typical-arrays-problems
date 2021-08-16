1e6bb719bbfa0ca2626fa623659f958c78973f2b
JacksonBaltramovich <e.baltramovich@mail.ru>
2021-08-17 01:15:42 +0300

    completed task typical array problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(!Array.isArray(array)||array.length===0){
+        return 0;
+      }
+    let minimum = array.sort((a,b) => a - b);
+        return minimum[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(!Array.isArray(array)||array.length===0){
+        return 0;
+      }
+      let maximum = array.sort((a,b) => b - a);
+        return maximum[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(!Array.isArray(array)||array.length===0){
+        return 0;
+      }
+      return array.reduce((a, b) => (a + b)) / array.length;
 }
